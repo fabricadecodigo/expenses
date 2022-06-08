0d83da5d20dea38b1a0b38a2372a6706300c695d
Felipe Franco <ffranco@fullstacklabs.co>
2022-06-08 17:14:21 -0300

    pwa

--- src/index.tsx
+++ src/index.tsx
@@ -34,7 +34,7 @@ root.render(
   </ApolloProvider>
 );
 
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
